ffb120d7192789f06560a8775def36c91a40dca6
gitdante <92villarreal@gmail.com>
2017-10-16 10:11:41 +0800

    script

---
+++ sd.py
@@ -0,0 +1,90 @@
+import gdal
+import numpy as np
+import os
+
+
+def getSRSPair(dataset):
+    '''
+    获得给定数据的投影参考系和地理参考系
+    :param dataset: GDAL地理数据
+    :return: 投影参考系和地理参考系
+    '''
+    prosrs = osr.SpatialReference()
+    prosrs.ImportFromWkt(dataset.GetProjection())
+    geosrs = prosrs.CloneGeogCS()
+    return prosrs, geosrs
+
+def geo2lonlat(dataset, x, y):
+    '''
+    将投影坐标转为经纬度坐标（具体的投影坐标系由给定数据确定）
+    :param dataset: GDAL地理数据
+    :param x: 投影坐标x
+    :param y: 投影坐标y
+    :return: 投影坐标(x, y)对应的经纬度坐标(lon, lat)
+    '''
+    prosrs, geosrs = getSRSPair(dataset)
+    ct = osr.CoordinateTransformation(prosrs, geosrs)
+    coords = ct.TransformPoint(x, y)
+    return coords[:2]
+
+def lonlat2geo(dataset, lon, lat):
+    '''
+    将经纬度坐标转为投影坐标（具体的投影坐标系由给定数据确定）
+    :param dataset: GDAL地理数据
+    :param lon: 地理坐标lon经度
+    :param lat: 地理坐标lat纬度
+    :return: 经纬度坐标(lon, lat)对应的投影坐标
+    '''
+    prosrs, geosrs = getSRSPair(dataset)
+    ct = osr.CoordinateTransformation(geosrs, prosrs)
+    coords = ct.TransformPoint(lon, lat)
+    return coords[:2]
+
+def imagexy2geo(dataset, row, col):
+    '''
+    根据GDAL的六参数模型将影像图上坐标（行列号）转为投影坐标或地理坐标（根据具体数据的坐标系统转换）
+    :param dataset: GDAL地理数据
+    :param row: 像素的行号
+    :param col: 像素的列号
+    :return: 行列号(row, col)对应的投影坐标或地理坐标(x, y)
+    '''
+    trans = dataset.GetGeoTransform()
+    px = trans[0] + row * trans[1] + col * trans[2]
+    py = trans[3] + row * trans[4] + col * trans[5]
+    return px, py
+
+def geo2imagexy(dataset, x, y):
+    '''
+    根据GDAL的六 参数模型将给定的投影或地理坐标转为影像图上坐标（行列号）
+    :param dataset: GDAL地理数据
+    :param x: 投影或地理坐标x
+    :param y: 投影或地理坐标y
+    :return: 影坐标或地理坐标(x, y)对应的影像图上行列号(row, col)
+    '''
+    trans = dataset.GetGeoTransform()
+    a = np.array([[trans[1], trans[2]], [trans[4], trans[5]]])
+    b = np.array([x - trans[0], y - trans[3]])
+    return np.linalg.solve(a, b)  # 使用numpy的linalg.solve进行二元一次方程的求解
+
+def where(path,lon,lat):
+    fid=gdal.Open(path, gdal.GA_ReadOnly)
+    x, y = lonlat2geo(fid, lon, lat)
+    r, c = geo2imagexy(fid, x, y)
+    return r,c
+
+def readdata(path,r,c,b,dict):
+    fid = gdal.Open(path, gdal.GA_ReadOnly)
+    for i in range(1,b+1):
+        data = fid.GetRasterBand(i).ReadAsArray()
+        dict[i].append(data[r,c])
+    return 1
+
+files=os.listdir('./')
+datas=[i for i in files if 'tif' in i]
+plon=100.372200
+plat=38.855500
+row,col=where(datas[0],plon,plat)
+bands=gdal.Open(datas[0], gdal.GA_ReadOnly).RasterCount
+dic={i:[] for i in range(1,bands+1)}
+for i in datas:
+    readdata(i,round(row),round(col),bands,dic)
